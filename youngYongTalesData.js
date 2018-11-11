b2a881858d54c1921ee75ce6b57432fb71529046
Zienigma <40841929+Zienigma@users.noreply.github.com>
2018-11-11 09:11:27 +0800

    First Commit

---
+++ youngYongTalesData.js
@@ -0,0 +1,14 @@
+var data = new Chartist.Bar('#youngYongTalesData', {
+  series: [
+[//it said data was already provided.?]
+}, {
+  fullWidth: true,
+  axisX: { showLabel: false, showGrid: false },
+  axisY: {
+    showLabel: false,
+    showGrid: false,
+    scaleMinSpace: 2
+  },
+  seriesBarDistance: 1,
+  low: 4500
+});
